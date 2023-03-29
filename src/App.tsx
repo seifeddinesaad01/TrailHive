c5430bcb514912376cc2d7019eb6c905e96c3c96
seifeddinesaad01 <saadseif1925@outlook.com>
2023-03-29 20:45:18 +0100

    git config issues

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
       <Navbar />
-      {/* <SectionOne /> */}
+      <SectionOne />
     </div>
   );
 }
