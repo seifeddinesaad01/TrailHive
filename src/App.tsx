a50c77bb7e59fa7cf1ef6373937dfad5255c6b96
seifeddinesaad01 <saadseif1925@outlook.com>
2023-03-29 20:47:10 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import Navbar from "./Components/NavBar/Navbar";
 import SectionOne from "./Components/Section-1/section-1";
 function App() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
-      <Navbar />
-      <SectionOne />
-    </div>
+    // <div style={{ display: "flex", flexDirection: "column", gap: "2rem" }}>
+    //   <Navbar />
+    //   <SectionOne />
+    // </div>
+    <p>saad</p>
   );
 }
 
