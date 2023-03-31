a5049c512d043a4a9d776be922292692d5112aac
seifeddinesaad <saadseif1925@outlook.com>
2023-03-31 18:28:45 +0100

    section 4

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
        <SectionTwo/> 
        <SectionThree />
      </div>
-    
   );
 }
+
 
 export default App;
--- src/Components/Section-3/Section-3.tsx
+++ src/Components/Section-3/Section-3.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SectionThree = () => {
   return (
-    <div><p>seif eddine saad</p></div>
+    <div className='section__three'>
+      <p className='title'>Testimonials</p>
+      <h2>"TrailHive has completely changed the way I explore the outdoors. The personalized trail recommendations and <br /> virtual challenges have kept me motivated and engaged <br /> with my adventures."</h2>
+      <p className='sub'>-seif eddine</p>
+    </div>
   )
 }
 
