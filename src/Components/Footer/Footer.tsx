be96335d670928f420759dc172b3d25eee66fe08
seifeddinesaad <saadseif1925@outlook.com>
2023-03-31 21:19:45 +0100

    footer first touches

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+   <div className="footer">
+    <h1>TrailHive</h1>
+    <ul>
+        <li>App</li>
+        <li>Feature</li>
+        <li>Benefits</li>
+        <li>Pricing</li>
+        <li>Explore</li>
+        <li>trails</li>
+    </ul>
+    <ul>
+        <li>Company</li>
+        <li>About</li>
+        <li>Jobs</li>
+        <li>Press</li>
+        <li>Support</li>
+        <li>Members</li>
+    </ul>
+   </div>
   )
 }
 
