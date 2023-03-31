138ad4b49689eece55da93d5acb0a14f7b331f33
seifeddinesaad <saadseif1925@outlook.com>
2023-03-31 21:05:48 +0100

    section 5

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import SectionOne from "./Components/Section-1/section-1";
 import { SectionTwo } from "./Components/Section-2";
 import { SectionThree } from "./Components/Section-3";
 import { SectionFour } from "./Components/Section-4";
+import { SectionFive } from "./Components/Section-5";
+import { Footer } from "./Components/Footer";
 function App() {
   return (
-     <div className="main__layout">
-       <Navbar />
-       <SectionOne />
-       <SectionTwo/> 
-       <SectionThree />
-        {/* <SectionFour/>  */}
-     </div>
+    <div className="main__layout">
+      <Navbar />
+      <SectionOne />
+      <SectionTwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+      <Footer />
+    </div>
   );
 }
 
--- src/Components/Section-5/Section-5.tsx
+++ src/Components/Section-5/Section-5.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const SectionFive = () => {
   return (
-    <div>SectionFive</div>
+    <div className="section__five">
+      {/* <div className="background">
+        <img src="https://wallpaperaccess.com/full/114712.jpg" alt="men and nature" />
+      </div> */}
+      <div className="content">
+      <p className='title'>Testimonials</p>
+      <h2>"I love the social networking aspect <br /> of TrailHive. It's great to connect <br /> with other hikers and <br /> bikers and plan group events together."</h2>
+      <p className='sub'>-seif eddine</p>
+      </div>
+    </div>
   )
 }
 
