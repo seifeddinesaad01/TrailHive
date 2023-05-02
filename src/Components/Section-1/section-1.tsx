28b9bdab89abbcc331bcfba9ca8161efc28829cd
seifeddinesaad01 <saadseif1925@outlook.com>
2023-05-02 11:39:05 +0100

    some background changes

--- src/Components/Section-1/section-1.tsx
+++ src/Components/Section-1/section-1.tsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import menInNature from "../../assets/OIP.jpeg"
+import React from "react";
+import menInNature from "../../assets/nature.jpg";
 const sectionOne = () => {
   return (
-    <section className='section__one'>
-        <div className="left">
-            <h1>Your Ultimate <br /> Outdoor <br /> Companion</h1>
-            <p>Are you tired of spending hours searching for the perfect hiking or biking trail? <br /> TrailHive - the ultimate outdoor adventure app that connects you with the best trails in your area.</p>
-            <button className='download__btn'>Download The App</button>
-        </div>
-        <div className="right">
-            <img src={menInNature} alt="menInNature" />
-        </div>
+    <section className="section__one">
+      <div className="left">
+        <h1>
+          Your Ultimate <br /> Outdoor <br /> Companion
+        </h1>
+        <p>
+          Are you tired of spending hours searching for the perfect hiking or
+          biking trail? <br /> TrailHive - the ultimate outdoor adventure app
+          that connects you with the best trails in your area.
+        </p>
+        <button className="download__btn">Download The App</button>
+      </div>
+      <div className="right">
+        <img src={menInNature} style={{ height: "30rem" }} alt="menInNature" />
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default sectionOne
+export default sectionOne;
--- src/Components/Section-4/Section-4.tsx
+++ src/Components/Section-4/Section-4.tsx
@@ -1,14 +1,16 @@
-import React from 'react'
-
+import React from "react";
+import natureImg from "../../assets/nature1.jpg";
 const SectionFour = () => {
   return (
-    <div className="section__four" id='Blog'>
+    <div className="section__four" id="Blog">
       <div className="section__title">
-        <p>Find new trails to export and <br /> adventure to embark on.</p>
+        <p>
+          Find new trails to export and <br /> adventure to embark on.
+        </p>
       </div>
-      <img src="https://i.pinimg.com/originals/92/ff/68/92ff683903770357e5458a913b85f64c.jpg" alt="" />
+      <img src={natureImg} alt="" />
     </div>
-  )
-}
+  );
+};
 
 export default SectionFour;
